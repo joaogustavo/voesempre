3d9f6125edbec2350d78f9abfdf564578020b57f
Joaogdf <joaogustavone@gmail.com>
2023-04-09 13:11:17 +0100

    commiting Voe Sempre website

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include ("../../portfolio-web.php"); ?>
+
+
+<script>
+
+function setSelectedIndex(s, v) {
+for ( var i = 0; i < s.options.length; i++ ) {
+if ( s.options[i].text == v ) {
+s.options[i].selected = true;
+return;
+}
+}
+}
+setSelectedIndex(document.getElementById('projetos'),"Voe Sempre");
+
+</script>
